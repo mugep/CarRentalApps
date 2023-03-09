33c7dc426365012d621c1dfb423843c47fcceae1
Muhamad Gerald <44803708+mugep@users.noreply.github.com>
2023-03-09 20:40:47 +0700

    Change naming of customer name and added clicked button submit

--- CarRentalApps/Form1.Designer.cs
+++ CarRentalApps/Form1.Designer.cs
@@ -30,7 +30,7 @@ namespace CarRentalApps
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbCustomerName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
@@ -52,13 +52,13 @@ namespace CarRentalApps
             this.label1.TabIndex = 0;
             this.label1.Text = "Car Rental";
             // 
-            // textBox1
+            // tbCustomerName
             // 
-            this.textBox1.Location = new System.Drawing.Point(9, 71);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(191, 20);
-            this.textBox1.TabIndex = 1;
+            this.tbCustomerName.Location = new System.Drawing.Point(9, 71);
+            this.tbCustomerName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbCustomerName.Name = "tbCustomerName";
+            this.tbCustomerName.Size = new System.Drawing.Size(191, 20);
+            this.tbCustomerName.TabIndex = 1;
             // 
             // label2
             // 
@@ -73,7 +73,7 @@ namespace CarRentalApps
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(9, 142);
+            this.dateTimePicker1.Location = new System.Drawing.Point(10, 143);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(191, 20);
@@ -81,7 +81,7 @@ namespace CarRentalApps
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(397, 142);
+            this.dateTimePicker2.Location = new System.Drawing.Point(398, 143);
             this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(191, 20);
@@ -143,6 +143,7 @@ namespace CarRentalApps
             this.button1.TabIndex = 9;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -157,7 +158,7 @@ namespace CarRentalApps
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbCustomerName);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
@@ -170,7 +171,7 @@ namespace CarRentalApps
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbCustomerName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
--- CarRentalApps/Form1.cs
+++ CarRentalApps/Form1.cs
@@ -16,5 +16,10 @@ namespace CarRentalApps
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thank you for Renting");
+        }
     }
 }
